92b8c57f3f6c7a27cc4698b7e004fc019d4a6fdd
Ajian920 <106301917+Ajian920@users.noreply.github.com>
2026-07-31 16:23:48 +0800

    feat: add main.js

---
+++ main.js
@@ -0,0 +1,31 @@
+﻿const { app, BrowserWindow, Menu } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    minWidth: 380,
+    minHeight: 600,
+    icon: path.join(__dirname, 'build', 'icon.png'),
+    title: '资源共享',
+    backgroundColor: '#0a0a12',
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true
+    }
+  });
+
+  Menu.setApplicationMenu(null);
+  win.loadFile('index.html');
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
